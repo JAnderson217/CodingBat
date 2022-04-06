049b13f3934c9f947de54068be49257bdfcda331
JAnderson217 <24385176+JAnderson217@users.noreply.github.com>
2022-04-06 18:28:36 +0100

    Recursion + Dictionary

---
+++ Maps/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> map = new Dictionary<string, string>
+            {
+            { "a", "candy" },
+            { "b", "dirt" }
+            };
+            map = mapBully(map);
+            foreach (KeyValuePair<string, string> x in map)
+            {
+                Console.WriteLine($"Key = {x.Key}, Value = {x.Value}");
+            }
+            Console.ReadLine();
+        }
+        public static Dictionary<string, string> mapBully(Dictionary<string, string> map)
+        {
+            if (map.ContainsKey("a"))
+            {
+                map["b"] = map["a"];
+                map["a"] = "";
+                //map.put("b", map.get("a"));
+                //map.put("a", "");
+            }
+            return map;
+        }
+    }
+}
--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -12,6 +12,7 @@ namespace Recursion
         {
             Console.WriteLine($"factorial test with 2,3,6: {factorial(2)}, {factorial(3)}, {factorial(6)}");
             Console.WriteLine($"countX test with xxhixx, xhixhix, x {countX("xxhixx")}, {countX("xhixhix")}, {countX("x")}");
+            Console.WriteLine($"splitArray test with [2,2], [2,3], [5,2,3] {splitArray(new int[] { 2, 2 })}, {splitArray(new int[] { 2, 3 })}, {splitArray(new int[] { 5, 2, 3 })}");
             Console.ReadLine();
         }
         static int factorial(int n)
@@ -35,5 +36,29 @@ namespace Recursion
             str = str.Substring(1);
             return count + countX(str);
         }
+        public static bool splitArray(int[] nums)
+        {
+            //return index, sum of left side, sum of right side, numbs - initially set to zero
+            return splitArrayHelper(0,0,0,nums);
+        }
+        public static bool splitArrayHelper(int i, int l, int r, int[] nums)
+        {
+            //check to end recursion, both sides have to be equal
+            if (i >= nums.Length)
+            {
+                return l == r;
+            }
+            //continue recursion, could be added to left or right side, so use or to check both instances, add 1 to index
+            return splitArrayHelper(i+1, l + nums[i], r, nums) || splitArrayHelper(i+1, l, r + nums[i], nums);
+/*            if (splitarrayhelper(i+1, l + nums[i], r, nums))
+            {
+                return true;
+            }
+            if (splitarrayhelper(i+1, l, r + nums[i], nums))
+            {
+                return true;
+            }
+            return false; */  
+        }   
     }
 }
